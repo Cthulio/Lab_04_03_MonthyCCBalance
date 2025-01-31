527a890dc26923e61333b4ea241353af4416f52e
Nolan Crone <crone.nolan@gmail.com>
2025-01-30 23:05:36 -0500

    Init commit and Part 2 Task 3 complete

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double cardDebt = 5000;
+        double interestRate = .17;
+        System.out.println("The total due the first month is: "+(cardDebt*(1+interestRate)));
+        System.out.println("The total due the second month is: "+((cardDebt*interestRate)*(1+interestRate)));
+    }
+}
